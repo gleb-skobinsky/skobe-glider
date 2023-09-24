711af53575cc91e9870a86b6b11fa05e62b8e3e7
gleb-skobinsky <glebgytnik@gmail.com>
2023-09-24 19:22:51 +0300

    Extract experience to separate component

---
+++ app/TechStack.tsx
@@ -0,0 +1,28 @@
+import GridIcon from "./gridIcon";
+import React from "react";
+import { poppins } from "./fonts";
+
+export function TechStack({}) {
+  return (
+    <div className="bg-slate-950 py-20 w-full px-[10%]">
+      <div className={`mb-20 text-3xl ${poppins.className}`}>
+        My experience in the industry:
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-20 gap-x-4">
+        <GridIcon src="/kotlin.svg" alt="Kotlin language" />
+        <GridIcon src="/kmp.svg" alt="Kotlin Multiplatform" />
+        <GridIcon src="/compose.png" alt="Compose Multiplatform" />
+        <GridIcon src="/ktor.svg" alt="Ktor library" />
+        <GridIcon src="/gradle.svg" alt="Gradle build tool" />
+        <GridIcon src="/docker.svg" alt="Docker" />
+        <GridIcon src="/nginx.svg" alt="Nginx" />
+        <GridIcon src="/python.svg" alt="Python" />
+        <GridIcon src="/csharp.svg" alt="C#" />
+        <GridIcon src="/blazor.png" alt="Blazor" />
+        <GridIcon src="/postgres.png" alt="PostgreSQL" />
+        <GridIcon src="/js.svg" alt="JavaScript" />
+        <GridIcon src="/vuejs.svg" alt="Vue.js" />
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import { TechStack } from "./TechStack";
 import { Description } from "./Description";
 import { MainSubTitle } from "./MainSubTitle";
 import { MainTitle } from "./MainTitle";
 import { AvatarImage } from "./AvatarImage";
 import { MobileMenu } from "./MobileMenu";
-import GridIcon from "./gridIcon";
 import { poppins } from "./fonts";
 
 export default function Home() {
@@ -36,26 +36,7 @@ export default function Home() {
       <MainTitle />
       <MainSubTitle />
       <AvatarImage />
-      <div className="bg-slate-950 py-20 w-full px-[10%]">
-        <div className={`mb-20 text-3xl ${poppins.className}`}>
-          My experience in the industry:
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-20 gap-x-4">
-          <GridIcon src="/kotlin.svg" alt="Kotlin language" />
-          <GridIcon src="/kmp.svg" alt="Kotlin Multiplatform" />
-          <GridIcon src="/compose.png" alt="Compose Multiplatform" />
-          <GridIcon src="/ktor.svg" alt="Ktor library" />
-          <GridIcon src="/gradle.svg" alt="Gradle build tool" />
-          <GridIcon src="/docker.svg" alt="Docker" />
-          <GridIcon src="/nginx.svg" alt="Nginx" />
-          <GridIcon src="/python.svg" alt="Python" />
-          <GridIcon src="/csharp.svg" alt="C#" />
-          <GridIcon src="/blazor.png" alt="Blazor" />
-          <GridIcon src="/postgres.png" alt="PostgreSQL" />
-          <GridIcon src="/js.svg" alt="JavaScript" />
-          <GridIcon src="/vuejs.svg" alt="Vue.js" />
-        </div>
-      </div>
+      <TechStack />
       <Description />
       <div className=""></div>
     </div>
