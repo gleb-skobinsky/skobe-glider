624d187162b42e003ad0c5313a05873ab7384c68
glebgutnik <glebgytnik@gmail.com>
2024-03-17 13:02:54 +0300

    Make tooltip mobile-compatible

--- app/[lang]/components/gridIcon.tsx
+++ app/[lang]/components/gridIcon.tsx
@@ -4,8 +4,16 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { yMotionVariant } from "./motionVariant";
 import { Tooltip } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function GridIcon(props: { src: string; alt: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const doOpen = () => {
+    setIsOpen(true);
+  };
+  const doClose = () => {
+    setIsOpen(false);
+  };
   return (
     <motion.div
       className="grid-item"
@@ -14,8 +22,21 @@ export default function GridIcon(props: { src: string; alt: string }) {
       exit="hide"
       variants={yMotionVariant}
     >
-      <Tooltip showArrow={true} color="primary" content={props.alt}>
-        <Image src={props.src} width={100} height={100} alt={props.alt} />
+      <Tooltip
+        isOpen={isOpen}
+        showArrow={true}
+        color="primary"
+        content={props.alt}
+      >
+        <Image
+          src={props.src}
+          width={100}
+          height={100}
+          alt={props.alt}
+          onClick={doOpen}
+          onMouseLeave={doClose}
+          onMouseEnter={doOpen}
+        />
       </Tooltip>
     </motion.div>
   );
